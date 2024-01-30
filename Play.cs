9aca40bd24b3f4eb8fa2d9fc3639f831c8dbc418
Hannahkj12 <hannahkj2001@gmail.com>
2024-01-30 08:41:34 -0700

    Add project files.

---
+++ Play.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IS413Mission4
+{
+    internal class Play
+    {
+    }
+}
+
+/*The supporting class (with whichever name you choose) will:
+• Receive the “board” array from the driver class
+• Contain a method that prints the board based on the information passed to it
+• Contain a method that receives the game board array as input and returns if there is a
+winner and who it was*/
